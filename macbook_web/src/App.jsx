6136c577bf8b949ae0e39aa3d4eeeafb8653ed37
yukesh-code25 <yukesh.codee@gmail.com>
2026-09-02 14:55:45 +0530

    Initial commit

---
+++ macbook_web/src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar.jsx"
+
+const App = () => {
+  return (
+    <main>
+        <NavBar />
+    </main>
+  )
+}
+
+export default App
---
+++ macbook_web/src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iphone" },
+  { label: "watch" },
+  { label: "vision" },
+  { label: "Airpods" },
+];
